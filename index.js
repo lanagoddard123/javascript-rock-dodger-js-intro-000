b2c32fd2eec40eaee27d51fd4fd7c1564fddb7ee
lanasgoddard <lanasgoddard@gmail.com>
2019-03-02 12:08:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -56,6 +56,7 @@ function createRock(x) {
 
   rock.style.top = top
 GAME.appendChild(rock)
+moveRock()
   /**
    * Now that we have a rock, we'll need to append
    * it to GAME and move it downwards.
